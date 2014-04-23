088407f778d33f9c555630687d7713c0f86dc75d
Richard Eames <reames@asymmetricventures.com>
2014-04-23 12:38:28 -0600

    Fixed setup

--- setup.py
+++ setup.py
@@ -31,15 +31,18 @@ License :: OSI Approved :: GNU General Public License v2 (GPLv2)
 Topic :: Software Development :: Libraries :: Application Frameworks
 """
 
+version = '0.1.0'
+url = 'https://github.com/AsymmetricVentures/asym-testing'
+
 setup(
 	name = 'asymmetricbase.testing',
-	version = datetime.now().strftime('%Y%m%d%H%M'),
-	url = 'https://github.com/AsymmetricVentures/asymmetricbase',
-	
+	version = '{}-{}'.format(version, datetime.now().strftime('%Y%m%d%H%M')),
+	url = url,
+	download_url = '{}/archive/v{}.tar.gz'.format(url, version),
 	author = 'Richard Eames',
 	author_email = 'reames@asymmetricventures.com',
 	packages = find_packages(),
-	classifiers = filter(None, classifiers.split('\n')),
+	classifiers = list(filter(None, classifiers.split('\n'))),
 	namespace_packages = ['asymmetricbase'],
 	
 	install_requires = (
@@ -53,7 +56,6 @@ setup(
 		
 		'asymmetricbase.views'
 	),
-	
 	package_dir = {
 		'asymmetricbase' : 	'asymmetricbase',
 	},
